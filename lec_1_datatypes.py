51a340baf886bd815130502c9891deb32a9704bf
svatoslav0567 <114597029+svatoslav0567@users.noreply.github.com>
2022-10-05 16:24:56 +0000

    ..

---
+++ lec_1_datatypes.py
@@ -0,0 +1,19 @@
+type
+
+type(3)
+
+print(type(3))
+
+print(type(3.4))
+
+print(type('Молодец'))
+
+a = 'Rock'
+print(type(a))
+
+a = ['Rock', 1, 'Ken', 6, 8]
+print(type(a))
+
+print(type(False))
+
+print(type(None))
---
+++ lec_1_list.py
@@ -0,0 +1,21 @@
+a = [1, 3, 6]
+print(a[0])
+
+b = [8, 10, 11]
+c = b + a
+print(c)
+
+b = [8, 10, 11]
+c = a + b
+print(*c)
+
+ # c = a - b
+
+print(c * 2)
+c.append('Good')
+print(c)
+
+a.append(b)
+print(a[3] [0])
+
+print(len(a))
